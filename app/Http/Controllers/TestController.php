69ce95c6285914cbaa781369a7ad11adc8502e5a
chitta1331 <chittaranjan@synergicsoftek.com>
2021-08-25 12:34:29 +0530

    test controller add

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -3,13 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class TestController extends Controller
 {
     public function Test(){
-        $invID=10;
-        return $invID = str_pad($invID, 4, '0', STR_PAD_LEFT);
+        // $invID=10;
+        // return $invID = str_pad($invID, 4, '0', STR_PAD_LEFT);
 
-        return md5(uniqid(rand(), true));
+        // return md5(uniqid(rand(), true));
+        $d=1234;
+        return Hash::make($d);
     }
 }
