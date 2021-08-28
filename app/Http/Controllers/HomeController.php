32aadb2530dc11ea657a2ab1195abda91e9810ce
chitta1331 <chittaranjan@synergicsoftek.com>
2021-08-28 18:22:25 +0530

    email send msg 4

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -24,8 +24,8 @@ class HomeController extends Controller
         $message=$request->message;
         // return $message;
         $email_id=app('App\Http\Controllers\HomeController')->EmailSendAddress();
-        return $email_id;
-        Mail::to($email_id)->send(new ContactUsAdminEmail($name,$email,$subject,$message));
+        // return $email_id;
+        Mail::to($request->email)->send(new ContactUsAdminEmail($name,$email,$subject,$message));
         Mail::to($request->email)->send(new ContactUsEmail($name,$subject, $message));
         return redirect()->route('index')->with('success','success');
     }
