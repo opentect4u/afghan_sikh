474ddcd1ab5984dd76c2c6e178afdb365f812c43
chitta1331 <chittaranjan@synergicsoftek.com>
2021-08-28 12:20:18 +0530

    test111

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -10,14 +10,18 @@ use App\Models\MdUserLogin;
 class TestController extends Controller
 {
     public function Test(){
+
+        $data=DB::table('mytest')->get();
+        return $data;
         // $invID=10;
         // return $invID = str_pad($invID, 4, '0', STR_PAD_LEFT);
 
         // return md5(uniqid(rand(), true));
         // $d=1234;
         // return Hash::make($d);
-        $gurudwara_email='testsc04@gmail.com';
-        $is_user=MdUserLogin::where('user_id',$gurudwara_email)->get();
-        return count($is_user);
+        // $gurudwara_email='testsc04@gmail.com';
+        // $is_user=MdUserLogin::where('user_id',$gurudwara_email)->get();
+        // return count($is_user);
     }
+    
 }
