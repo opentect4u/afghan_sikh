ee45534ababd637bc7d0ee21197e55c248a67f6f
chitta1331 <chittaranjan@synergicsoftek.com>
2021-08-28 19:20:29 +0530

    email send msg 0

--- resources/views/emails/contact-us-admin.blade.php
+++ resources/views/emails/contact-us-admin.blade.php
@@ -65,7 +65,7 @@ p{font-size:13px; line-height:25px; margin:0; padding:0;}
     
     <tr>
       <td align="left" valign="top">
-      <p style="font-size:13px; line-height:24px; margin:0 0 20px 0; padding:0;"><strong>Subject : </strong> {{$subject1}}</p>
+      <p style="font-size:13px; line-height:24px; margin:0 0 20px 0; padding:0;"><strong>Subject : </strong> {{$message}}</p>
         </td>
       </tr>
     <tr>
