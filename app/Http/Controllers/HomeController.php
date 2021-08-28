290515ef63ff7e4e593ed9639b0f342c39988470
chitta1331 <chittaranjan@synergicsoftek.com>
2021-08-28 19:01:53 +0530

    email send msg 0

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -22,7 +22,7 @@ class HomeController extends Controller
         $email=$request->email;
         $subject1=$request->subject;
         $message=$request->message;
-        return $message;
+        // return $message;
         $email_id=app('App\Http\Controllers\HomeController')->EmailSendAddress();
         // return $email_id;
         Mail::to($email_id)->send(new ContactUsAdminEmail($name,$email,$subject1,$message));
