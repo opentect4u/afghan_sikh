0534874706d9204c2084a3a5b9f3b4017b80167a
chitta1331 <chittaranjan@synergicsoftek.com>
2021-08-28 18:18:16 +0530

    email send msg 1

--- resources/views/emails/contact-us-admin.blade.php
+++ resources/views/emails/contact-us-admin.blade.php
@@ -65,7 +65,7 @@ p{font-size:13px; line-height:25px; margin:0; padding:0;}
     
     <tr>
       <td align="left" valign="top">
-          <p style="font-size:13px; line-height:24px; margin:0 0 20px 0; padding:0;">Message : {{jason_decode($message)}}</p>
+          <p style="font-size:13px; line-height:24px; margin:0 0 20px 0; padding:0;">Message : {{json_decode($message)}}</p>
         </td>
       </tr>
     <tr>
