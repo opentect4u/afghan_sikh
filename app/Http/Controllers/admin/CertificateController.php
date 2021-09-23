d79332fbb30cb11f594f54b1989e993252c7a239
chitta1331 <chittaranjan@synergicsoftek.com>
2021-09-23 11:15:32 +0530

    fixing 4

--- app/Http/Controllers/admin/CertificateController.php
+++ app/Http/Controllers/admin/CertificateController.php
@@ -149,7 +149,7 @@ class CertificateController extends Controller
         $gurudwara=DB::table('md_user_login')
                     ->leftJoin('td_gurudwara_details', 'md_user_login.id', '=', 'td_gurudwara_details.id')
                     ->select('md_user_login.*', 'td_gurudwara_details.*') 
-                    ->Where('md_user_login.user_type','G')               
+                    ->whereIn('md_user_login.user_type', array('G', 'O', 'C'))
                     ->Where('md_user_login.active','A')               
                     // ->orderBy('td_user_details.updated_at', 'desc')
                     ->get();
